1cb87219c4e3eff98a947282048711ce8928acb8
Rafael Garcia <rafael.garcia.ocampo@swedbank.se>
2025-12-05 12:14:38 +0100

    add: day5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import day1.Day1
 import day2.Day2
 import day3.Day3
+import day5.Day5
 
 fun main() {
 //    val day1 = Day1(Day1.day1Input)
@@ -9,7 +10,10 @@ fun main() {
 //    val day2 = Day2(Day2.input)
 //    println("Part 1: ${day2.part1()}")
 //    println("Part 2: ${day2.part2()}")
-    val day3 = Day3(Day3.example)
-    println("Part 1: ${day3.part1()}")
-    println("Part 2: ${day3.part2()}")
+//    val day3 = Day3(Day3.example)
+//    println("Part 1: ${day3.part1()}")
+//    println("Part 2: ${day3.part2()}")
+    val day5 = Day5(Day5.example)
+    //println("Part 1: ${day5.part1()}")
+    println("Part 2: ${day5.part2()}")
 }
---
+++ src/main/kotlin/day5/Day5.kt
@@ -0,0 +1,78 @@
+package day5
+
+class Day5(
+    private val input: List<String>
+) {
+
+    companion object {
+
+        @Suppress("unused")
+        val example = """3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32""".trimIndent().lines()
+
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        val day5Input = javaClass
+            .getResourceAsStream("/day5_input.txt")
+            ?.bufferedReader(Charsets.UTF_8)
+            ?.lines()
+            ?.toList()
+            .orEmpty()
+    }
+
+    fun part1(): Int {
+        val freshIds = input.subList(0, input.indexOf("")).map {
+            val range = it.split('-')
+            val from = range[0].toLong()
+            val to = range[1].toLong()
+            from..to
+        }
+        val availableIds = input.subList(input.indexOf("") + 1, input.size).map { str -> str.toLong() }
+        val x = availableIds.mapNotNull { availableId: Long ->
+            val isInFreshIds = freshIds.any { freshIdRange -> availableId in freshIdRange }
+            if (isInFreshIds) {
+                availableId
+            } else {
+                null
+            }
+        }
+        return x.count()
+    }
+
+    fun part2(): Long {
+        val freshIdsRanges = input.subList(0, input.indexOf("")).map {
+            val range = it.split('-')
+            val from = range[0].toLong()
+            val to = range[1].toLong()
+            from..to
+        }.sortedBy { idRange -> idRange.first }
+
+        val merged = mutableListOf<LongRange>()
+        for (range in freshIdsRanges) {
+            if (merged.isEmpty() || merged.last().last < range.first - 1) {
+                merged.add(range)
+            } else {
+                // the current range overlaps or is adjacent to the last merged range,
+                // so update the last merged range to cover both by extending its end to the maximum of the two ends.
+                merged[merged.lastIndex] = merged.last().first..maxOf(merged.last().last, range.last)
+            }
+        }
+
+        val totalUniqueCount = merged
+            .sumOf {
+                println("counting range: $it, first: ${it.first} last: ${it.last} diff: ${it.last - it.first + 1}")
+                it.last - it.first + 1
+            }
+        println("Total unique count: $totalUniqueCount")
+        return totalUniqueCount
+    }
+}
+
